4e38315a537b91184db814cab6edf8ccb50590ba
muskan regmi <muskanregmi@gmail.com>
2015-08-13 09:36:16 -0700

    Pushing to Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VeterinaryPractice::Application.config.secret_token = '2b5dcadcdc00e841898ecc8f2bc9691204820001c5091dabb1945878ae38e089d99eda25bf60cbd69a0db5a57be89d6c4ab2350c714a1e1adc9c4bd9052c1179'
